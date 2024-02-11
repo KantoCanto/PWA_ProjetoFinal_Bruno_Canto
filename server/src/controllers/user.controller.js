ab32dd4477ff623f6a1fe3ef761240171b935f7d
KantoCanto <bruno.canto.neves@gmail.com>
2024-02-11 10:05:11 +0000

    added user controller

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,112 @@
+import userModel from '../models/user.model';
+import jsonwebtoken from 'jsonwebtoken';
+import responseHandler from '../handlers/response.handler';
+
+
+const register = async (req, res) => {
+    try{
+        const { username, password, displayName } = req.body;
+
+        const checkUser = await userModel.findOne({username});
+        if(checkUser){
+            return responseHandler.badrequest(res, "username already exists")
+        }
+
+        const user = new userModel()
+
+        user.displayName = displayName;
+        user.username = username;
+        user.setPassword(password);
+
+        await user.save();
+        
+        const token = jsonwebtoken.sign(
+            {data: user._id},
+            process.env.TOKEN_SECRET,
+            {expiresIn: "24h"}
+        );
+
+        responseHandler.created(res, {
+            token,
+            ...user._doc,
+            id: user._id
+        });
+    }catch{
+        responseHandler.error(res)
+    }
+}
+
+const login = async (req, res) => {
+    try{
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne({username}).select("username password salt id dislayName");
+
+        if(!user){
+            return responseHandler.badrequest(res, "Username not found.")
+        };
+
+        if(!user.validPassword(password)){
+            return(
+                responseHandler.badrequest(res, "Incorrect Password")
+            )
+        };
+
+        const token = jsonwebtoken.sign(
+            {data: user._id},
+            process.env.TOKEN_SECRET,
+            {expiresIn: "24h"}
+        );
+
+        user.password = undefined;
+        user.salt = undefined;
+
+        responseHandler.created(res, {
+            token,
+            ...user._doc,
+            id: user._id
+        });
+
+    }catch{
+        responseHandler.error(res)
+    }
+}
+
+const updatePassword = async (req, res) => {
+    try{
+        const { password, newPassword } = req.body;
+
+        const user = await userModel.findById(req.user.id).select("password salt");
+
+        if(!user){
+            return responseHandler.unauthorized(res);
+        }
+
+        if(!user.validPassword(password)){
+            return responseHandler.badrequest(res, "Incorrect Password")
+        }
+
+        user.setPassword(newPassword);
+        await user.save();
+
+        responseHandler.ok(res, "Password changed successfully")
+    }catch{
+        responseHandler.error(res)
+    }
+}
+
+const getInfo = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.user.id);
+
+        if(!user){
+            return responseHandler.notfound(res);
+        }
+
+        responseHandler.ok(res, user);
+    }catch{
+        responseHandler.error(res)
+    }
+}
+
+export default { register, login, getInfo, updatePassword };
