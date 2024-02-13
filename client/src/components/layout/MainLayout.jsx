b924c7a8248226687ca95476408fe10d33d25d77
KantoCanto <bruno.canto.neves@gmail.com>
2024-02-13 14:12:02 +0000

    created circutlar rate component

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -13,6 +13,33 @@ import { setListFavorites, setUser } from "../../redux/features/userSlice";
 
 const MainLayout = () => {
 
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userApi.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { response, err } = await favoriteApi.getList();
+
+      if (response) dispatch(setListFavorites(response));
+      if (err) toast.error(err.message);
+    };
+
+    if (user) getFavorites();
+    if (!user) dispatch(setListFavorites([]));
+  }, [user, dispatch]);
+
   return (
     <>
 
